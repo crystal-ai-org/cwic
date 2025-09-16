1812e4264393d4436f4aec04c5fbe6f4af51d70b
Cyris Kissane <cyris@kissane.com>
2025-09-16 18:03:52 +0000

    more evals

---
+++ cwic/eval_all.sh
@@ -0,0 +1,49 @@
+
+python evaluate.py \
+    --model "../checkpoints/llama-1B_1B-tokens_THAWED-HEAD/00003000_2p05x" \
+    --batch_size 16 \
+    --max_length 1024 \
+    --max_samples 1000
+
+python evaluate.py \
+    --model "../checkpoints/llama-1B_1B-tokens_THAWED-HEAD/00006000_3p05x" \
+    --batch_size 16 \
+    --max_length 1024 \
+    --max_samples 1000
+
+python evaluate.py \
+    --model "../checkpoints/llama-1B_1B-tokens_THAWED-HEAD/00009000_4p05x" \
+    --batch_size 16 \
+    --max_length 1024 \
+    --max_samples 1000
+
+python evaluate.py \
+    --model "../checkpoints/llama-1B_1B-tokens_THAWED-HEAD/00012000_5p05x" \
+    --batch_size 16 \
+    --max_length 1024 \
+    --max_samples 1000
+
+python evaluate.py \
+    --model "../checkpoints/llama-1B_1B-tokens_THAWED-HEAD/00015000_6p05x" \
+    --batch_size 16 \
+    --max_length 1024 \
+    --max_samples 1000
+
+python evaluate.py \
+    --model "../checkpoints/llama-1B_1B-tokens_THAWED-HEAD/00018000_6p05x" \
+    --batch_size 16 \
+    --max_length 1024 \
+    --max_samples 1000
+
+python evaluate.py \
+    --model "../checkpoints/llama-1B_1B-tokens_THAWED-HEAD/00021000_6p05x" \
+    --batch_size 16 \
+    --max_length 1024 \
+    --max_samples 1000
+
+python evaluate.py \
+    --model "../checkpoints/llama-1B_1B-tokens_THAWED-HEAD/00024000_6p05x" \
+    --batch_size 16 \
+    --max_length 1024 \
+    --max_samples 1000
+    
